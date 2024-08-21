8d25eacb41c6c1ed0e64d3555c5e89b0d6ec220f
NoOseGG <hoc751@gmail.com>
2024-08-21 18:28:43 +0300

    added all zodiacs

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,10 +1,63 @@
+import aquarius from "../images/aquarius.svg";
+import aries from "../images/aries.svg";
+import cancer from "../images/cancer.svg";
+import capricorn from "../images/capricorn.svg";
+import gemini from "../images/gemini.svg";
+import leo from "../images/leo.svg";
+import libra from "../images/libra.svg";
+import pisces from "../images/pisces.svg";
+import sagitarius from "../images/sagitarius.svg";
+import scorpio from "../images/scorpio.svg";
+import taurus from "../images/taurus.svg";
+import virgo from "../images/virgo.svg";
+
 export const zodiacs = [
   {
-    name: "Козерог",
-    img: "https://pngimg.com/uploads/capricorn/capricorn_PNG59.png",
+    name: "Aquarius",
+    img: aquarius,
   },
   {
-    name: "Водолей",
-    img: "https://cdn0.iconfinder.com/data/icons/zodiac-signs-14/100/Artboard_10-1024.png",
+    name: "Aries",
+    img: aries,
+  },
+  {
+    name: "Cancer",
+    img: cancer,
+  },
+  {
+    name: "Capricorn",
+    img: capricorn,
+  },
+  {
+    name: "Gemini",
+    img: gemini,
+  },
+  {
+    name: "Leo",
+    img: leo,
+  },
+  {
+    name: "Libra",
+    img: libra,
+  },
+  {
+    name: "Pisces",
+    img: pisces,
+  },
+  {
+    name: "Sagitarius",
+    img: sagitarius,
+  },
+  {
+    name: "Scorpio",
+    img: scorpio,
+  },
+  {
+    name: "Taurus",
+    img: taurus,
+  },
+  {
+    name: "Virgo",
+    img: virgo,
   },
 ];
